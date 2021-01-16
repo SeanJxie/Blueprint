15bf205abf5fb7bc412ebb8a6dca17edf5a733a9
Sean Xie <47431127+SeanJxie@users.noreply.github.com>
2021-01-16 14:44:36 -0500

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -8,8 +8,8 @@ import util
 A shape constructor
 
 TODO: Navigation
-      Hide grid
-
+      Elements to clases
+      Fix hard coded gui value
 """
 
 
@@ -22,9 +22,9 @@ pg.font.init()
 DISPLAY_SIZE  = util.get_display_size()
 WIN_NAME      = "Blueprint"  # Program operates in fullscreen so...
 
-MAINSURFACE    = pg.display.set_mode((0, 0), pg.FULLSCREEN)
-DEFAULT_FONT   = pg.font.SysFont("ariel", 50)
-MAX_FRAME_RATE = 60
+MAINSURFACE     = pg.display.set_mode((0, 0), pg.FULLSCREEN)
+ACTIVE_GUI_FONT = pg.font.SysFont("ariel", 50)
+MAX_FRAME_RATE  = 60
 
 # A cell size of 1 or 2 would make the computer not happy :(
 MIN_CELL_SIZE   = 3
@@ -80,41 +80,35 @@ def draw_all_shapes(lst):
 def draw_line_preview(start, mPos):
      pg.draw.line(MAINSURFACE, GREEN, start, mPos, DRAW_LINE_WIDTH)
 
-     len_gui = DEFAULT_FONT.render(f"len={round(util.distance(start, mPos), CURSOR_POS_ROUND_TO)}", True, WHITE)
+     len_gui = ACTIVE_GUI_FONT.render(f"len={round(util.distance(start, mPos), CURSOR_POS_ROUND_TO)}", True, WHITE)
      MAINSURFACE.blit(len_gui, (mPos[0] + CURSOR_RAD, mPos[1]))
 
 
 def draw_circle_preview(center, r, mPos):
     pg.draw.circle(MAINSURFACE, GREEN, center, r, DRAW_CIRCLE_WIDTH)
 
-    radius_gui = DEFAULT_FONT.render(f"r: {round(r, CURSOR_POS_ROUND_TO)}", True, WHITE)
+    radius_gui = ACTIVE_GUI_FONT.render(f"r: {round(r, CURSOR_POS_ROUND_TO)}", True, WHITE)
     MAINSURFACE.blit(radius_gui, (mPos[0] + CURSOR_RAD, mPos[1]))
 
 
-def gui(draw_mode, mPos):
+def menu_gui():
+    menu_img = pg.image.load("../assets/menu_img.png")
+    menu_img = pg.transform.scale(menu_img, DISPLAY_SIZE)
+    MAINSURFACE.fill(WHITE)
+    MAINSURFACE.blit(menu_img, (0, 0))
+
+
+def active_gui(draw_mode, mPos):
     if draw_mode == E_TYPE_LINE:
-        draw_mode_gui = DEFAULT_FONT.render("DRAW MODE: LINE"              , True, BLACK, WHITE)
+        draw_mode_gui = ACTIVE_GUI_FONT.render("DRAW MODE: LINE"              , True, RED, WHITE)
     elif draw_mode == E_TYPE_CIRCLE:
-        draw_mode_gui = DEFAULT_FONT.render("DRAW MODE: CIRCLE"            , True, BLACK, WHITE)
-    switch_line_gui   = DEFAULT_FONT.render('LINE: "L"'                    , True, BLACK, WHITE)
-    switch_circle_gui = DEFAULT_FONT.render('CIRCLE: "C"'                  , True, BLACK, WHITE)
-    hide_gui_gui      = DEFAULT_FONT.render('HIDE GUI: "H"'                , True, BLACK, WHITE)
-    cell_size_gui     = DEFAULT_FONT.render('CHANGE CELL SIZE: "UP"/"DOWN"', True, BLACK, WHITE)
-    undo_mode_gui     = DEFAULT_FONT.render('UNDO: "RIGHT MOUSE BUTTON"'   , True, BLACK, WHITE)
-    clear_gui         = DEFAULT_FONT.render('CLEAR: "MIDDLE MOUSE BUTTON"' , True, BLACK, WHITE)
-    exit_gui          = DEFAULT_FONT.render('EXIT: "ESC"'                  , True, BLACK, WHITE)
-    mouse_pos_gui     = DEFAULT_FONT.render(f'CURSOR POS: {mPos}'          , True, BLACK, WHITE)
+        draw_mode_gui = ACTIVE_GUI_FONT.render("DRAW MODE: CIRCLE"            , True, RED, WHITE)
+
+    mouse_pos_gui     = ACTIVE_GUI_FONT.render(f'CURSOR POS: {mPos}'          , True, BLACK, WHITE)
 
     # TODO Positions are hard-coded
-    MAINSURFACE.blit(draw_mode_gui    , (0, 0  ))
-    MAINSURFACE.blit(switch_line_gui  , (0, 100))
-    MAINSURFACE.blit(switch_circle_gui, (0, 200))
-    MAINSURFACE.blit(hide_gui_gui     , (0, 300))
-    MAINSURFACE.blit(cell_size_gui    , (0, 400))
-    MAINSURFACE.blit(undo_mode_gui    , (0, 500))
-    MAINSURFACE.blit(clear_gui        , (0, 600))
-    MAINSURFACE.blit(exit_gui         , (0, 700))
-    MAINSURFACE.blit(mouse_pos_gui    , (0, 800))
+    MAINSURFACE.blit(draw_mode_gui, (0, 0  ))
+    MAINSURFACE.blit(mouse_pos_gui, (0, 33))
 # --- End functions ---
 
 
@@ -123,8 +117,12 @@ def main():
     pg.display.set_caption(WIN_NAME)
 
     mousePos = [0, 0]
-    preview = False
-    show_gui = True
+
+    # GUI vars
+    preview   = False
+    show_menu = True # This statement is scattered around. Defines menu state and active program state.
+    show_gui  = True
+    show_grid = True
 
     # Stores all shape data in the form (v1, v2, draw_type)
     draw_list = []
@@ -132,7 +130,7 @@ def main():
     # Stores a drawable element in the form (v1, v2, draw_type)
     element = []
 
-    # Default draw mode is line mode
+    # ACTIVE_GUI draw mode is line mode
     draw_mode = E_TYPE_LINE
 
     # Start index of zoom cycle
@@ -149,93 +147,109 @@ def main():
                     sys.exit(0)
 
                 # Toggle gui
-                elif event.key == pg.K_h:
-                    show_gui = not show_gui
+                elif event.key == pg.K_m:
+                    show_menu = not show_menu
 
-                # Increase and decrease cell size
-                elif event.key == pg.K_UP:
-                    grid_zoom_idx = (grid_zoom_idx + 1) % len(GRID_ZOOM_CYCLE)
+                if not show_menu:
+                    # Increase and decrease cell size
+                    if event.key == pg.K_UP:
+                        grid_zoom_idx = (grid_zoom_idx + 1) % len(GRID_ZOOM_CYCLE)
 
-                elif event.key == pg.K_DOWN:
-                    grid_zoom_idx = (grid_zoom_idx - 1) % len(GRID_ZOOM_CYCLE)
+                    elif event.key == pg.K_f:
+                        show_gui = not show_gui
 
+                    elif event.key == pg.K_DOWN:
+                        grid_zoom_idx = (grid_zoom_idx - 1) % len(GRID_ZOOM_CYCLE)
 
-                # Only allow draw mode changes if not in preview mode. Otherwise, stuff messes up.
-                else: 
-                    if event.key == pg.K_l:
+                    elif event.key == pg.K_l:
                         draw_mode = E_TYPE_LINE
+                        
                     elif event.key == pg.K_c:
                         draw_mode = E_TYPE_CIRCLE
+
+                    elif event.key == pg.K_g:
+                        show_grid = not show_grid
                 
             # User presses mouse button
             elif event.type == pg.MOUSEBUTTONDOWN:
-                button = pg.mouse.get_pressed() # Returns (middle, right, left) boolean tuple
+                if not show_menu:
+                    button = pg.mouse.get_pressed() # Returns (middle, right, left) boolean tuple
 
-                # User places point at cursor
-                if button[0]: # Left
+                    # User places point at cursor
+                    if button[0]: # Left
 
-                    if draw_mode == E_TYPE_LINE:
+                        if draw_mode == E_TYPE_LINE:
 
-                        # User defines start. Activate preview
-                        if len(element) == 0:
-                            element.append(mousePos)
-                            preview = True
+                            # User defines start. Activate preview
+                            if len(element) == 0:
+                                element.append(mousePos)
+                                preview = True
 
-                        elif len(element) == 1:            # User has placed second point
-                            element.append(mousePos)       # Add point to element
-                            element.append(E_TYPE_LINE)    # Add element type
+                            elif len(element) == 1:            # User has placed second point
+                                element.append(mousePos)       # Add point to element
+                                element.append(E_TYPE_LINE)    # Add element type
 
-                            # Check if element already exists.
-                            if element not in draw_list:
-                                draw_list.append(element)  # Add element to draw list
+                                # Check if element already exists.
+                                if element not in draw_list:
+                                    draw_list.append(element)  # Add element to draw list
 
-                            preview = False                # Exit preview
-                            element = []                   # Reset element
+                                preview = False                # Exit preview
+                                element = []                   # Reset element
 
 
-                    elif draw_mode == E_TYPE_CIRCLE:
+                        elif draw_mode == E_TYPE_CIRCLE:
 
-                        # User defines center. Activate preview.
-                        if len(element) == 0:
-                            element.append(mousePos)
-                            preview = True
+                            # User defines center. Activate preview.
+                            if len(element) == 0:
+                                element.append(mousePos)
+                                preview = True
 
-                        # Similar process to line. Except second data value is a radius.
-                        elif len(element) == 1:
-                            element.append(util.distance(mousePos, element[0]))
-                            element.append(E_TYPE_CIRCLE)
-                            if element not in draw_list:
-                                draw_list.append(element)
-                            preview = False
-                            element = []
-                        
+                            # Similar process to line. Except second data value is a radius.
+                            elif len(element) == 1:
+                                element.append(util.distance(mousePos, element[0]))
+                                element.append(E_TYPE_CIRCLE)
+                                if element not in draw_list:
+                                    draw_list.append(element)
+                                preview = False
+                                element = []
+                            
 
-                elif button[2]: # Right
-                    if preview:          # If in preview mode
-                        preview = False  # Exit preview
-                        element = []     # Reset element
+                    elif button[2]: # Right
+                        if preview:          # If in preview mode
+                            preview = False  # Exit preview
+                            element = []     # Reset element
 
-                    elif draw_list:       # If not in preview mode and line is not empty
-                        draw_list.pop(-1) # Remove latest element
+                        elif draw_list:       # If not in preview mode and line is not empty
+                            draw_list.pop(-1) # Remove latest element
 
-                elif button[1]: # Middle 
-                    draw_list = [] # Clear 
+                    elif button[1]: # Middle 
+                        draw_list = [] # Clear 
 
-        # Get modified mouse position
-        mousePos = snap_to_grid(GRID_ZOOM_CYCLE[grid_zoom_idx], pg.mouse.get_pos())
+        if not show_menu:
+            # Get modified mouse position
+            mousePos = snap_to_grid(GRID_ZOOM_CYCLE[grid_zoom_idx], pg.mouse.get_pos())
 
-         # --- Start draw functions ---
-        MAINSURFACE.fill(BLUEPRINT_BLUE)
-        draw_base_grid(GRID_ZOOM_CYCLE[grid_zoom_idx])
-        draw_all_shapes(draw_list)
-        if preview:
-            if draw_mode == E_TYPE_LINE:
-                draw_line_preview(element[0], mousePos)
-            elif draw_mode == E_TYPE_CIRCLE:
-                draw_circle_preview(element[0], util.distance(mousePos, element[0]), mousePos)
-        if show_gui:
-            gui(draw_mode, mousePos)
-        draw_cursor(mousePos)
+            # --- Start draw functions ---
+            MAINSURFACE.fill(BLUEPRINT_BLUE)
+            if show_grid:
+                draw_base_grid(GRID_ZOOM_CYCLE[grid_zoom_idx])
+
+            draw_all_shapes(draw_list)
+
+            if preview:
+                if draw_mode == E_TYPE_LINE:
+                    draw_line_preview(element[0], mousePos)
+
+                elif draw_mode == E_TYPE_CIRCLE:
+                    draw_circle_preview(element[0], util.distance(mousePos, element[0]), mousePos)
+
+            if show_gui:
+                active_gui(draw_mode, mousePos)
+
+            draw_cursor(mousePos)
+
+        else:
+            menu_gui()
         # --- End draw functions ---
 
         # Update display
